578ae669b082bc8123f66582b43ef0d9f5693576
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-01-29 16:37:47 +0500

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -2,16 +2,8 @@
 
 # navigate to app folder
 cd app
-
-# initial startup by running react-script "start", name process "marketing"
-# --watch watches and restarts if files change
 pm2 start
-
-# auto restart server if shut down
-pm2 startup
-
 # freeze process list for automatic respawn
 pm2 save
-
 # restart all processes - necessary to do this again?
 pm2 restart all
