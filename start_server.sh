36897f14a8c935ff3c0293741532e876e760c32f
unknown <hassanirtaza7006@gmail.com>
2023-01-31 11:16:37 +0500

    main: Deleted File

--- start_server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo starting server
-# navigate to app folder
-cd var/www/
-echo building application...
-sudo npm run build
-echo starting application...
-sudo pm2 start npm -- run production
