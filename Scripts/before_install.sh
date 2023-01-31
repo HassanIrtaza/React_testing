b84776fb9f20e4b5766429c20059aec473488c21
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-01-31 19:31:24 +0500

    Update before_install.sh

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # navigate to app folder
-#cd /home/ubuntu/home/myapp
-sudo apt install nginx
+cd /home/ubuntu/home/myapp
 # install node and npm
 curl -fsSL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
-sudo apt-get install -y nodejs
+sudo -y install nodejs npm
