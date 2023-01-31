0f48a66c6ffc441671905d99b716fbf8b5c705e5
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-01-31 15:29:18 +0500

    Update start_server.sh

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo starting server
 # navigate to app folder
-cd /home/ubuntu
+cd /home/ubuntu/home/myapp
 echo starting application...
 sudo pm2 start npm
 sudo systemctl restart nginx
