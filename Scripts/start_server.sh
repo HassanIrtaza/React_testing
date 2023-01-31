3fcc485ff32a6297e5ab37073876ea197e951f69
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-01-31 15:06:20 +0500

    Update start_server.sh

--- Scripts/start_server.sh
+++ Scripts/start_server.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo starting server
 # navigate to app folder
-cd /app
-echo building application...
-sudo npm run build
+cd /home/ubuntu
 echo starting application...
 sudo pm2 start npm
