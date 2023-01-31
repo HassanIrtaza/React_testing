f4be81c9acf97be5227bfa6afbb1305c3a660189
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-01-31 17:39:27 +0500

    Update after_install.sh

--- Scripts/after_install.sh
+++ Scripts/after_install.sh
@@ -4,10 +4,11 @@
 cd /home/ubuntu/home/myapp
 
 # install dependencies
-npm i --force
+sudo npm i --force
 # install create-react-app and react-scripts
 # without react-scripts application cannot be started
-npm install --save create-react-app react-scripts
+sudo npm install --save create-react-app react-scripts
 
 # install pm2 process manager
-npm install pm2 -g
+sudo npm install pm2 -g
+sudo npm run build
