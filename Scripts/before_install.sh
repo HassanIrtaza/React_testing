d262cc2d87f3bf8f14ea207e55064fe4a7d79407
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-02-01 16:50:13 +0500

    Update before_install.sh

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -6,5 +6,4 @@ cd /home/ubuntu/home/myapp
 sudo curl -fsSL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
 sudo apt-get install -y nodejs
-sudo apt install -y nginx
 npm install pm2 -g
