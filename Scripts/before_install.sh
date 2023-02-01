979922bccbf39e8aa6edd952d2c06013658a27a2
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-02-01 06:28:13 +0500

    Update before_install.sh

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -5,4 +5,5 @@ cd /home/ubuntu/myapp
 # install node and npm
 curl -fsSL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 sudo bash /tmp/nodesource_setup.sh
+sudo npm install
 npm i --force
