8a97d94382c0b7bfa8c8cb96ccb9c4137ac14b14
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-02-03 12:35:17 +0500

    Update before_install.sh

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -6,4 +6,5 @@ cd /home/ubuntu/home/workspace/react/
 curl -fsSL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 bash /tmp/nodesource_setup.sh
 apt-get install -y nodejs
+
 #npm install pm2 -g
