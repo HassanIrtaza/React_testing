f5d5361ff4635c945c68af50f7da782660a77b8e
Hassan Irtaza <122802631+HassanIrtaza@users.noreply.github.com>
2023-02-06 13:59:38 +0500

    Update before_install.sh

--- Scripts/before_install.sh
+++ Scripts/before_install.sh
@@ -2,6 +2,7 @@
 
 # navigate to app folder
 cd /home/ubuntu/home/workspace/react/
+echo "nstalling"
 # install node and npm
 curl -fsSL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
 bash /tmp/nodesource_setup.sh
